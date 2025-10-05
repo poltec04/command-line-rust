1e9180edcfecd3e617b25ea3c78fe47498a25244
poltec04 <polytec0204@gmail.com>
2025-10-05 12:08:04 +0900

    chore(echor): start from empty main.rs

--- 02_echor/src/main.rs
+++ 02_echor/src/main.rs
@@ -1,23 +1,3 @@
-use clap::Parser;
-
-#[derive(Debug, Parser)]
-#[command(author, version, about)]
-/// Rust version of `echo`
-struct Args {
-    /// Input text
-    #[arg(required(true))]
-    text: Vec<String>,
-
-    /// Do not print newline
-    #[arg(short('n'))]
-    omit_newline: bool,
-}
-
 fn main() {
-    let args = Args::parse();
-    print!(
-        "{}{}",
-        args.text.join(" "),
-        if args.omit_newline { "" } else { "\n" }
-    );
+// TODO: clap(derive)で引数を定義してechoを実装
 }
