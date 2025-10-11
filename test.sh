41bf8b3d835565725935da03ae174d18f410e6c8
poltec04 <polytec0204@gmail.com>
2025-10-11 16:35:11 +0900

    modified test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+cargo fmt --all -- --check
 
 for DIR in [01]*; do
     DIRNAME=$(basename "$DIR")
     echo "==> $DIRNAME <=="
-    (cd $DIR && cargo test -q > /dev/null && cargo clippy)
+    (cd "$DIR" && cargo test -q > /dev/null && cargo clippy -- -D warnings)
 done
 
 echo "Done."
